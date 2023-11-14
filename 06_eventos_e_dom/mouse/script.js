7a0403f240f97943bf01d230683a3e0a294cac15
Pedro Lucas <izielucas2016@gmail.com>
2023-11-14 08:20:44 -0300

    criado eventos mouse e teclado

---
+++ 06_eventos_e_dom/mouse/script.js
@@ -0,0 +1,27 @@
+function acao1(){
+    document.getElementById("box1").style.background = "blue";
+}
+
+function acao2(){
+    document.getElementById("box1").style.background = "red";
+}
+
+function acao3(){
+    document.getElementById("box1").style.background = "green";
+}
+
+function acao4(){
+    document.getElementById("box1").style.background = "yellow";
+}
+
+function acao5(){
+    document.getElementById("box1").style.background = "black";
+}
+
+function acao01(){
+    document.getElementById("box2").style.background = "yellow";
+}
+
+function acao02(){
+    document.getElementById("box2").style.background = "black";
+}
---
+++ 06_eventos_e_dom/teclado/script.js
@@ -0,0 +1,3 @@
+function acao1(){
+    alert("Hello World")
+}
