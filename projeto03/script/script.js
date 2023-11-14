8ebde3c57fd6f03df22ba072af4f86d6730f3677
Pedro Lucas <izielucas2016@gmail.com>
2023-11-14 12:44:59 -0300

    projeto 3

--- projeto03/script/script.js
+++ projeto03/script/script.js
@@ -14,14 +14,19 @@ function verificaMulta(){
     if(multa == true){
         res.innerHTML = "Foi aplicada multa de: "
     }else{
+        res.className = "green";
         res.innerHTML = "Não foi aplicada multa"
+        
     }
 
     if(velocidade <= (1.2*60) && velocidade > 60 || velocidade < 30){
+        val.className = "red";
         val.innerHTML = "R$ 130,16 + 4 Pontos na CNH"
     }else if(velocidade > (1.2*60) && velocidade <= (1.5*60)){
+        val.className = "red";
         val.innerHTML = "R$ 195,23 + 5 Pontos na CNH"
     }else if(velocidade > (1.5*60)){
+        val.className = "red";
         val.innerHTML = "R$ 880,41 + 7 Pontos na CNH"
     }else{
         val.innerHTML = " "
